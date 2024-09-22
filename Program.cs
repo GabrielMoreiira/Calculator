8a3d2ffff16e4c27406353c2e72ed32ca81237a0
GabrielMoreira <144186063+GabrielMoreiira@users.noreply.github.com>
2024-09-22 20:50:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplicacao();
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float soma = v1 + v2;
+
+            Console.WriteLine($"O valor total e de : {soma}");
+            Console.ReadKey();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+
+            Console.WriteLine($"O resultado da substracao e {resultado}");
+
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+
+            Console.WriteLine($"O resultado da divisao e {resultado}");
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+
+            Console.WriteLine($"O resultado da multiplicacao e {resultado}");
+        }
+    }
+}
